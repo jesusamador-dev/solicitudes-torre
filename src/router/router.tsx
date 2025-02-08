45041ebb55152c728c346b2b700e8ada17ee450e
Jesús Guadalupe Macías Amador <47731561+jesusamador-dev@users.noreply.github.com>
2025-02-08 08:01:23 -0600

    Update router.tsx

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,7 +4,7 @@ import MainLayout from 'mf_mesacyc_dashboards_common/MainLayout';
 import ErrorLayout from 'mf_mesacyc_dashboards_common/ErrorLayout';
 import DSSpinner from 'mf_mesacyc_dashboards_common/DSSpinner';
 import Reencauces from '@/presentation/pages/Reencauces';
-import RouterReencacues from './RouterReencauces';
+import RouterReencauces from './RouterReencauces';
 
 const Dashboard = lazy(() => import('../presentation/pages/Dashboard'))
 
